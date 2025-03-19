ec8af3a30928e0ecf46e0588eba4dff54f6fc0c1
PandoCheng <107836498+PandoCheng@users.noreply.github.com>
2025-03-20 04:56:52 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -1,7 +1,7 @@
 export const LOBE_CHAT_CLOUD = 'PandoChat Cloud';
 
 export const BRANDING_NAME = 'PandoChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = 'https://xn--csso2npqovv3a.com/favicon.svg';
 
 export const ORG_NAME = 'PandoRun';
 
